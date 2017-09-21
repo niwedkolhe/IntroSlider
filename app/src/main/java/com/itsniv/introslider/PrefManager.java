9e48a0b369aebfc403f0fec13329cd0e47aedae6
niwedkolhe <nivedkolhe1992@gmail.com>
2017-09-22 00:08:35 +0530

    Initial commit

---
+++ app/src/main/java/com/itsniv/introslider/PrefManager.java
@@ -0,0 +1,37 @@
+package com.itsniv.introslider;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by it's niv on 24-08-2017.
+ */
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "its_niv-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+}
